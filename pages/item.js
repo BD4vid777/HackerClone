950469e0eae1010f870f38da05f39f496578432f
Dawid <64140934+BD4vid777@users.noreply.github.com>
2021-07-29 11:50:40 +0200

    Formatting comments + adding active link underline

--- pages/item.js
+++ pages/item.js
@@ -1,6 +1,7 @@
 import view from "../utils/view.js";
 import baseUrl from "../utils/baseUrl.js";
 import Story from "../components/Story.js";
+import Comment from "../components/Comment.js";
 
 export default async function Item() {
 
@@ -23,7 +24,7 @@ export default async function Item() {
     view.innerHTML = `
     <div>${Story(story)}</div>
     <hr/>
-    ${hasComments ? story.comments.map(comment => JSON.stringify(comment)).join('') : 'No comments'}
+    ${hasComments ? story.comments.map(comment => Comment(comment)).join('') : 'No comments'}
     `
 }
 
